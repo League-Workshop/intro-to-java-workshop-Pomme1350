4fabc80eb61e49f08ef191b787eaea0efbbfe4c6
Pomme1350 <43727674+Pomme1350@users.noreply.github.com>
2018-11-20 10:10:42 -0800

    updating

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,46 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot r2d2=new Robot();
+	r2d2.setSpeed(1000);
+	r2d2.setPenColor(Color.magenta);
+	r2d2.penDown();
 	
-		
+	
+	for(int i=0; i<20; i++) {
+	
+		r2d2.move(100);
+		r2d2.turn(100);
 		
 	}
+	r2d2.setPenColor(Color.CYAN);
+	
+	r2d2.move(200);
+	
+	
+	
+	for(int i=0; i<20; i++) {
+		
+		r2d2.move(100);
+		r2d2.turn(100);
+		
+	}
+		
+	r2d2.turn(300);
+	r2d2.setPenColor(Color.BLACK);
+	r2d2.move(150);
+	
+	
+	for(int i=0; i<20; i++) {
+		
+		r2d2.move(100);
+		r2d2.turn(100);
+		
+	}
+
+	r2d2.turn(270);
+	r2d2.setPenColor(Color.MAGENTA);
+	r2d2.move(50);
+	}
 }
+
